a636953f5460ba23ec41dfdc0d58435482d76df4
MarioGT <mario@mariogt.com>
2024-10-09 12:31:17 -0300

    Update setup_cmd_cl_compiler.bat

--- setup_cmd_cl_compiler.bat
+++ setup_cmd_cl_compiler.bat
@@ -1,7 +1,7 @@
 @echo off
 
 REM put this on a cmd shortcut and set target to:
-REM c:\Windows\System32\cmd.exe /k w:\shell.bat
+REM c:\Windows\System32\cmd.exe /k your_drive:\path_to_your\setup_cmd_cl_compiler.bat
 REM subst w: D:\dev
 
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
